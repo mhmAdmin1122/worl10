7220e7009f51bd9f2f3a2e96c77acc86896f80cc
mhmAdmin1122 <hasaanad12min@gmail.com>
2024-03-20 14:37:17 +0500

    error removes

--- pages/Components/A/AboutMainSec.tsx
+++ pages/Components/A/AboutMainSec.tsx
@@ -1,21 +1,32 @@
+// import React from 'react'
+
+// const AboutMainSec = () => {
+//   document.addEventListener('DOMContentLoaded', function() {
+//     const cursor = document.getElementById('cursor') as HTMLElement;
+
+//     document.addEventListener('mousemove', function(e: MouseEvent) {
+//         const x: number = e.clientX;
+//         const y: number = e.clientY;
+//         cursor.style.left = x + 'px';
+//         cursor.style.top = y + 'px';
+//         (document.getElementById('container') as HTMLElement).style.backgroundPosition = (-x/10) + 'px ' + (-y/10) + 'px'; /* Background ka movement */
+//     });
+// });
+//   return (
+//     <div  id="container">
+//       <div className="cursor Hero-Sec-About flex flex-col items-center justify-center">
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default AboutMainSec
 import React from 'react'
 
 const AboutMainSec = () => {
-  document.addEventListener('DOMContentLoaded', function() {
-    const cursor = document.getElementById('cursor') as HTMLElement;
-
-    document.addEventListener('mousemove', function(e: MouseEvent) {
-        const x: number = e.clientX;
-        const y: number = e.clientY;
-        cursor.style.left = x + 'px';
-        cursor.style.top = y + 'px';
-        (document.getElementById('container') as HTMLElement).style.backgroundPosition = (-x/10) + 'px ' + (-y/10) + 'px'; /* Background ka movement */
-    });
-});
   return (
-    <div  id="container">
-      <div className="cursor Hero-Sec-About flex flex-col items-center justify-center">
-      </div>
+    <div>
+      
     </div>
   )
 }
